a8c3039fae6508a4d24079ecd486d64eb25e7ee0
Kerem Deen <kerem.deen@gmail.com>
2023-10-03 10:47:01 -0400

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [item for item in num_list if item % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentence = [string+"!" for string in sentence_list if type(string)==type("asd")]
+    return new_sentence
+    
